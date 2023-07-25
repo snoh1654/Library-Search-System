46434e43ff9ed1185712973b6379c53d94ed853c
SeanN <seannoh1654@yahoo.ca>
2023-07-25 11:33:55 -0700

    Implemented Search Functionality

--- BookManager/urls.py
+++ BookManager/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("/author/<str:name>", views.searchAuthor, name="search-author"),
-    path("/title/<str:name>", views.searchTitle, name="search-title")
+    path("edit-books", views.editBooks, name="edit"),
+    path("search-title", views.searchTitle, name="search-title"),
+    path("search-author", views.searchAuthor, name="search-author")
+
 ]
--- BookManager/views.py
+++ BookManager/views.py
@@ -8,43 +8,113 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def index(request):
-    books = Book.objects.all()
-
+def bookHelper(request, books):
     bookInformation = {}
 
     for book in books:
         bookInformation[book.title] = {
-            author: [],
-            genre: [],
+            "author": [],
+            "genre": [],
         }
 
     authors = Author.objects.all()
     genres = Genre.objects.all()
 
     for author in authors:
+        if (str(author.book) in bookInformation):
+            bookInformation[str(author.book)]["author"].append(author.name)
+
         # add author name to book by the name of author.book
-        bookInformation[author.book].author.append(author.name)
 
     for genre in genres:
-        # add genre name to genre.book
-        bookInformation[genre.book].genre.append(genre.genre)
+        if (str(genre.book) in bookInformation):
+            bookInformation[str(genre.book)]["genre"].append(genre.genre)
 
-    return render(request, "BookManager/index.html", {
+        # add genre name to genre.book
+
+    return {
         "books": books,
         "bookInformation": bookInformation
-    })
+    }
 
 
-def searchAuthor(request, name):
-    books = Book.objects.filter(author=name)
-    return render(request, "BookManager/index.html", {
-        "books": books
-    })
+def sortBookInformation(request, books, bookInformation):
+    authors = []
+    genres = []
+
+    for book in books:
+        authors.append(bookInformation[book.title]["author"])
+        genres.append(bookInformation[book.title]["author"])
+
+    return {
+        "booksInfo": zip(books, authors, genres),
+        "authors": authors,
+        "genres": genres
+    }
 
 
-def searchTitle(request, name):
-    books = Book.objects.filter(title=name)
-    return render(request, "BookManager/index.html", {
-        "books": books
-    })
+def index(request):
+    returnBook = bookHelper(request, Book.objects.all())
+    context = sortBookInformation(request,
+                                  returnBook["books"], returnBook["bookInformation"])
+
+    return render(request, "BookManager/index.html", context)
+
+
+def searchAuthor(request):
+
+    if request.method == "GET":
+        query = request.GET.get("fname")
+
+        if (query == ""):
+            return index(request)
+
+        print(query)
+
+        bookHelperReturn = bookHelper(request, Book.objects.all())
+        books = bookHelperReturn["books"]
+        bookInformation = bookHelperReturn["bookInformation"]
+
+        filterBooks = []
+
+        for book in bookInformation.copy():
+            if query in bookInformation[book]["author"]:
+                filterBooks.append(book)
+            else:
+                bookInformation.pop(book)
+
+        books = Book.objects.filter(title__in=filterBooks)
+
+        print(books, "done")
+        print(bookInformation)
+
+        returnBook = {
+            "books": books,
+            "bookInformation": bookInformation
+        }
+
+        context = sortBookInformation(request,
+                                      returnBook["books"], returnBook["bookInformation"])
+
+        return render(request, "BookManager/index.html", context)
+    else:
+        return index(request)
+
+
+def searchTitle(request):
+    if request.method == "GET":
+        query = request.GET.get("title")
+
+        if (query == ""):
+            return index(request)
+
+        returnBook = bookHelper(request, Book.objects.filter(title=query))
+        context = sortBookInformation(request,
+                                      returnBook["books"], returnBook["bookInformation"])
+        return render(request, "BookManager/index.html", context)
+    else:
+        return index(request)
+
+
+def editBooks(request):
+    return index(request)
