87436cf5061515c482e1dae7b878535dc7522b05
SeanN <seannoh1654@yahoo.ca>
2023-07-25 21:33:29 -0700

    Created Helper Functions and Refactored Code

--- BookManager/views.py
+++ BookManager/views.py
@@ -31,14 +31,14 @@ def bookDictionaryInit(books):
 
     for author in authors:
         book = str(author.book)
-        if (book in bookInformation):
+        if book in bookInformation:
             bookData = bookInformation[book]
             bookAuthorList = bookData["author"]
             bookAuthorList.append(author.name)
 
     for genre in genres:
         book = str(genre.book)
-        if (book in bookInformation):
+        if book in bookInformation:
             bookData = bookInformation[book]
             bookGenreList = bookData["genre"]
             bookGenreList.append(genre.genre)
@@ -122,45 +122,12 @@ def editBooks(request):
 
 def addBook(request):
     if request.method == 'POST':
-        bookForm = BookForm(request.POST)
-        authorForm = AuthorForm(request.POST)
-        genreForm = GenreForm(request.POST)
+        result = performAdd(request)
 
-        if bookForm.is_valid() and authorForm.is_valid() and genreForm.is_valid():
-            new_title = bookForm.cleaned_data['title']
-            new_pages = bookForm.cleaned_data['pages']
-            new_publisher = bookForm.cleaned_data['publisher']
-            new_quote = bookForm.cleaned_data["quote"]
-            new_name = authorForm.cleaned_data["name"]
-            new_genre = genreForm.cleaned_data["genre"]
-
-            new_book = Book(
-                title=new_title,
-                pages=new_pages,
-                publisher=new_publisher,
-                quote=new_quote,
-            )
-
-            new_author = Author(
-                name=new_name,
-                book=new_book
-            )
-
-            new_genre = Genre(
-                genre=new_genre,
-                book=new_book
-            )
-
-            new_book.save()
-            new_author.save()
-            new_genre.save()
+        if result:
             return render(request, 'BookManager/add.html', {
                 'success': True
             })
-        else:
-            bookForm = BookForm()
-            genreForm = GenreForm()
-            authorForm = AuthorForm()
 
     return render(request, 'BookManager/add.html', {
         "bookForm": BookForm(),
@@ -169,22 +136,59 @@ def addBook(request):
     })
 
 
+def performAdd(request):
+    bookForm = BookForm(request.POST)
+    authorForm = AuthorForm(request.POST)
+    genreForm = GenreForm(request.POST)
+
+    if bookForm.is_valid() and authorForm.is_valid() and genreForm.is_valid():
+        new_title = bookForm.cleaned_data['title']
+        new_pages = bookForm.cleaned_data['pages']
+        new_publisher = bookForm.cleaned_data['publisher']
+        new_quote = bookForm.cleaned_data["quote"]
+        new_name = authorForm.cleaned_data["name"]
+        new_genre = genreForm.cleaned_data["genre"]
+
+        new_book = Book(
+            title=new_title,
+            pages=new_pages,
+            publisher=new_publisher,
+            quote=new_quote,
+        )
+        new_book.save()
+
+        authorNameList = new_name.split(",")
+        for authorName in authorNameList:
+            authorName.strip()
+            creatingAuthor = Author(name=authorName, book=new_book)
+            creatingAuthor.save()
+
+        genreList = new_genre.split(",")
+        for genre in genreList:
+            genre.strip()
+            creatingGenre = Genre(genre=genre, book=new_book)
+            creatingGenre.save()
+        return True
+    else:
+        return False
+
+
 def updateBook(request, id):
     if request.method == 'POST':
-        book = Book.objects.get(pk=id)
-        bookForm = BookForm(request.POST, instance=book)
-
-        if bookForm.is_valid():
-            bookForm.save()
+        result = performAdd(request)
+        if result:
+            book = Book.objects.get(pk=id)
+            book.delete()
             return render(request, 'BookManager/update.html', {
-                'form': bookForm,
                 'success': True
             })
     else:
         book = Book.objects.get(pk=id)
         bookForm = BookForm(instance=book)
     return render(request, 'BookManager/update.html', {
-        'form': bookForm
+        "bookForm": bookForm,
+        "authorForm": AuthorForm(),
+        "genreForm": GenreForm()
     })
 
 
