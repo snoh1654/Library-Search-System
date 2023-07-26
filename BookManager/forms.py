69ed266a2a7949e176d9c267e11b9421eb762e35
SeanN <seannoh1654@yahoo.ca>
2023-07-25 18:24:58 -0700

    Included Basic CSS and Finished Templates

--- BookManager/forms.py
+++ BookManager/forms.py
@@ -1,18 +1,27 @@
 from django import forms
-from .models import Book
-
-title = None
+from .models import Book, Author, Genre
 
 
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'pages', 'publisher', 'quote']
+        fields = ["title", "pages", "publisher", "quote"]
         labels = {
-            'title': 'Book Title',
-            'pages': 'Page Count',
-            'publisher': 'Publisher',
-            'quote': 'Quote'
+            "title": "Book Title",
+            "pages": "Page Count",
         }
 
-# class AuthorForm()
+
+class AuthorForm(forms.ModelForm):
+    class Meta:
+        model = Author
+        fields = ["name"]
+        labels = {
+            "name": "Author Name"
+        }
+
+
+class GenreForm(forms.ModelForm):
+    class Meta:
+        model = Genre
+        fields = ["genre"]
--- BookManager/views.py
+++ BookManager/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from .models import Book, Author, Genre
-from .forms import BookForm
+from .forms import BookForm, AuthorForm, GenreForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 # Create your views here.
 
 
-def bookHelper(request, books):
+def bookHelp(books):
     bookInformation = {}
 
     for book in books:
@@ -36,7 +36,7 @@ def bookHelper(request, books):
     }
 
 
-def sortBookInformation(request, books, bookInformation):
+def sortBookInformation(books, bookInformation):
     authors = []
     genres = []
 
@@ -52,15 +52,14 @@ def sortBookInformation(request, books, bookInformation):
 
 
 def index(request):
-    returnBook = bookHelper(request, Book.objects.all())
-    context = sortBookInformation(request,
-                                  returnBook["books"], returnBook["bookInformation"])
+    returnBook = bookHelp(Book.objects.all())
+    context = sortBookInformation(
+        returnBook["books"], returnBook["bookInformation"])
 
     return render(request, "BookManager/index.html", context)
 
 
 def searchAuthor(request):
-
     if request.method == "GET":
         query = request.GET.get("fname")
 
@@ -69,7 +68,7 @@ def searchAuthor(request):
 
         print(query)
 
-        bookHelperReturn = bookHelper(request, Book.objects.all())
+        bookHelperReturn = bookHelp(Book.objects.all())
         books = bookHelperReturn["books"]
         bookInformation = bookHelperReturn["bookInformation"]
 
@@ -91,8 +90,8 @@ def searchAuthor(request):
             "bookInformation": bookInformation
         }
 
-        context = sortBookInformation(request,
-                                      returnBook["books"], returnBook["bookInformation"])
+        context = sortBookInformation(
+            returnBook["books"], returnBook["bookInformation"])
 
         return render(request, "BookManager/index.html", context)
     else:
@@ -106,18 +105,18 @@ def searchTitle(request):
         if (query == ""):
             return index(request)
 
-        returnBook = bookHelper(request, Book.objects.filter(title=query))
-        context = sortBookInformation(request,
-                                      returnBook["books"], returnBook["bookInformation"])
+        returnBook = bookHelp(Book.objects.filter(title=query))
+        context = sortBookInformation(
+            returnBook["books"], returnBook["bookInformation"])
         return render(request, "BookManager/index.html", context)
     else:
         return index(request)
 
 
 def editBooks(request):
-    returnBook = bookHelper(request, Book.objects.all())
-    context = sortBookInformation(request,
-                                  returnBook["books"], returnBook["bookInformation"])
+    returnBook = bookHelp(Book.objects.all())
+    context = sortBookInformation(
+        returnBook["books"], returnBook["bookInformation"])
 
     return render(request, "BookManager/edit.html", context)
 
@@ -125,27 +124,49 @@ def editBooks(request):
 def addBook(request):
     if request.method == 'POST':
         bookForm = BookForm(request.POST)
-        if bookForm.is_valid():
+        authorForm = AuthorForm(request.POST)
+        genreForm = GenreForm(request.POST)
+
+        if bookForm.is_valid() and authorForm.is_valid() and genreForm.is_valid():
             new_title = bookForm.cleaned_data['title']
             new_pages = bookForm.cleaned_data['pages']
             new_publisher = bookForm.cleaned_data['publisher']
-            new_quote = bookForm.cleaned_data['quote']
+            new_quote = bookForm.cleaned_data["quote"]
+            new_name = authorForm.cleaned_data["name"]
+            new_genre = genreForm.cleaned_data["genre"]
 
-            new_student = Book(
+            new_book = Book(
                 title=new_title,
                 pages=new_pages,
                 publisher=new_publisher,
                 quote=new_quote,
             )
-            new_student.save()
+
+            new_author = Author(
+                name=new_name,
+                book=new_book
+            )
+
+            new_genre = Genre(
+                genre=new_genre,
+                book=new_book
+            )
+
+            new_book.save()
+            new_author.save()
+            new_genre.save()
             return render(request, 'BookManager/add.html', {
-                'form': BookForm(),
                 'success': True
             })
         else:
             bookForm = BookForm()
+            genreForm = GenreForm()
+            authorForm = AuthorForm()
+
     return render(request, 'BookManager/add.html', {
-        'form': BookForm()
+        "bookForm": BookForm(),
+        "authorForm": AuthorForm(),
+        "genreForm": GenreForm()
     })
 
 
@@ -169,7 +190,6 @@ def updateBook(request, id):
 
 
 def deleteBook(request, id):
-    print(id)
     if request.method == "POST":
         book = Book.objects.get(pk=id)
         book.delete()
